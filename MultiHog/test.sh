9f7973e673a2dcb622e1bf607d4fefac243e3a9f
luca abeni <luca.abeni@santannapisa.it>
2017-02-01 08:52:17 +0100

    Improve the MultiHog test
    Now test different values of maximum utilisation; also use "top" to display the task utilization

--- MultiHog/test.sh
+++ MultiHog/test.sh
@@ -1,7 +1,20 @@
 IDS=$(seq 1 11)
 
-for D in $IDS
+do_test() {
+  for D in $IDS
+   do
+    sudo ./dlhog 2 &
+   done
+  top -b -d 1 -n 10 | grep "dlhog"
+  sudo killall dlhog
+  sleep 1
+}
+
+BS=$(seq 200000 50000 950000)
+
+for B in $BS
  do
-  sudo ./d 2 &
+  echo "Testing $B 1000000..."
+  sudo sh -c "echo $B > /proc/sys/kernel/sched_rt_runtime_us"
+  do_test
  done
-pidstat 1 10; sudo killall d
